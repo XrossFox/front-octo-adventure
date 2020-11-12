c778e73b40463743059633bb25a3b422ffb95f85
David del Real <warhead_1090@hotmail.com>
2020-11-12 17:43:21 -0600

    Mapping is ready. Made a method that generates a play field. Probably ging to copy paste that in the back end

--- front-octo-adventure/src/app/play-field-grid/play-field-grid.component.ts
+++ front-octo-adventure/src/app/play-field-grid/play-field-grid.component.ts
@@ -18,110 +18,86 @@ export class PlayFieldGridComponent implements OnInit {
     this.fGrid.columns = environment.columns;
     this.fGrid.rows = environment.rows;
 
-    for(let i = 0; i < this.fGrid.rows; i++){
-      this.fGrid.FieldGrid.push([]);
-
-      for(let j = 0; j < this.fGrid.columns; j++){
-        if(i == 0)
-          this.fGrid.FieldGrid[i][j] = map.pic1;
-        else{
-          let x = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
-
-          switch(x){
-            case 1:{
-              this.fGrid.FieldGrid[i][j] = map.pic2;
-              break;
-            }
-            case 2:{
-              this.fGrid.FieldGrid[i][j] = map.pic3;
-              break;
-            }
-            case 3:{
-              this.fGrid.FieldGrid[i][j] = map.pic4;
-              break;
-            }
-            case 4:{
-              this.fGrid.FieldGrid[i][j] = map.pic3;
-              break;
-            }
-          }
-        }
-      }
-    }
-
+    this.generateFieldGrid();
     console.log(this.fGrid.FieldGrid);
-
   }
 
     /**
    * Please note that this method is only for testing purposes. The front must not know anything about how to generate
    * a play field, only to sisplay it. That is the back-end's job.
    * 
+   * i'll probably just copy paste this into the back-end or something like that
+   * 
    * Generates a random play field grid. Sets te borders accordingly and whatever is in between is randomized.
    */
   private generateFieldGrid() {
+
+    // tiles that are floor
+    let floorTiles : string [] = []; 
+    floorTiles.push(map.floorGray);
+    floorTiles.push(map.floorDirt1);
+    floorTiles.push(map.floorDirt2);
+    floorTiles.push(map.floorStone1);
+    floorTiles.push(map.floorStone2);
 
     // insert columns
     for(let x = 0; x < this.fGrid.columns; x++){
       this.fGrid.FieldGrid.push([]);
 
       for(let y = 0; y < this.fGrid.rows; y++){
-        
+
         // if it's a upper corner tile, either left or right
-        if((x == 0 && y == 0) || (x == this.fGrid.columns && y == 0)){
+        if((x == 0 && y == 0) || (x == 0 && y == this.fGrid.rows-1)){
           // its the left one
-          if(x == 0)
-            this.fGrid.FieldGrid[x][y] == map.borderUpLeft;
+          if(y == 0)
+            this.fGrid.FieldGrid[x].push(map.borderUpLeft);
           // its the right one
           else 
-            this.fGrid.FieldGrid[x][y] == map.borderUpRight;
+            this.fGrid.FieldGrid[x].push(map.borderUpRight);
           // don't forget the continue to skip to next cycle, otherwise, it might collido with the forthcoming ifs
           continue;
         }
-
+        
         // if it's a down corner tile, either left or right
-        if((x == 0 && y == this.fGrid.rows-1) || (x == this.fGrid.columns && y == this.fGrid.rows)){
-          if(x == 0)
-            this.fGrid.FieldGrid[x][y] == map.borderDownLeft;
+        if((x == this.fGrid.columns-1 && y == 0) || (x == this.fGrid.columns-1 && y == this.fGrid.rows-1)){
+          if(y == 0)
+            this.fGrid.FieldGrid[x].push(map.borderDownLeft);
           else
-            this.fGrid.FieldGrid[x][y] == map.borderDownRight;
+            this.fGrid.FieldGrid[x].push(map.borderDownRight);
           continue;
         }
-
+        
         // if it's the upper row, but not the corners
-        if((x > 0 || x < this.fGrid.columns) && y == 0){
-          this.fGrid.FieldGrid[x][y] == map.borderUpMid;
+        if((x == 0) && (y > 0 && y < this.fGrid.rows-1)){
+          this.fGrid.FieldGrid[x].push(map.borderUpMid);
           continue;
         }
-
+        
+        
         // if it's the lower row, but not the corners
-        if((x > 0 || x < this.fGrid.columns) && y == this.fGrid.rows-1){
-          this.fGrid.FieldGrid[x][y] == map.borderUpMid;
+        if((x == this.fGrid.columns-1) && (y > 0 && y < this.fGrid.rows-1)){
+          this.fGrid.FieldGrid[x].push(map.borderDownMid);
           continue;
         }
 
         // if it's the left/right border but not of the first/last row. weirdest-ass if ever. might as well do ML with this shit
         if(
-          (x == 0 && (y > 0 || y < this.fGrid.rows-1)) // x is 0, so its left border, but y is neither 0 (first row) or the last row
+          (y == 0 && (x > 0 && x < this.fGrid.columns-1)) // y is 0, so its left border, but x is neither 0 (first row) or the last row
           || // or
-          (x == this.fGrid.rows-1 && (y > 0 || y < this.fGrid.rows-1)) // same as above, just this time, it's the right border 
+          (y == this.fGrid.rows-1 && (x > 0 && x < this.fGrid.columns-1)) // same as above, just this time, it's the right border 
           ){
-              if(x == 0) // left border
-                this.fGrid.FieldGrid[x][y] == map.borderLeft;
+              if(y == 0) // left border
+                this.fGrid.FieldGrid[x].push(map.borderLeft);
               else // right border
-                this.fGrid.FieldGrid[x][y] == map.borderRight;
+                this.fGrid.FieldGrid[x].push(map.borderRight);
               continue; // just in case
           }
-        
+
         // if you rechead this point, it means you not anywhere in the upper row, lower row, first column nor last column
         // so go full yolo and pick a tile randomly from the floor tiles.
-
+        this.fGrid.FieldGrid[x].push(floorTiles[Math.floor(Math.random() * floorTiles.length)]);
       }
-
     }
-    
-    
-    
   }
 
   ngOnInit(): void {
